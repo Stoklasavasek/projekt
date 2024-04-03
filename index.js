4f77e54fda73a12df85f8a68888c3ee0c83425dd
Stoklasavasek <118250112+Stoklasavasek@users.noreply.github.com>
2024-04-03 21:02:29 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public')); // pro statické soubory (CSS, JS)
